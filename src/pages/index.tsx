2e2cc9fdf0a20e1ca3fa2e7d670fe4bfbfb3bc52
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2020-12-25 02:13:54 -0300

    :wrench: update file-structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+
+export default function Home() {
+	return (
+		<Layout>
+			<Head>
+				<title>Rixa</title>
+			</Head>
+			<h1>Hello World</h1>
+		</Layout>
+	);
+}
