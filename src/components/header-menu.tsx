ca0e2e47cd5c73609a46669a13e6a475ca27cd96
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2021-01-05 00:28:26 -0300

    :refactor: update Menu component to a Drawer

--- src/components/header-menu.tsx
+++ src/components/header-menu.tsx
@@ -3,42 +3,58 @@ import {
 	Flex,
 	Box,
 	Menu,
-	MenuList,
-	MenuItem,
 	MenuButton,
 	Button,
 	ButtonGroup,
 	Divider,
 	Icon,
+	Drawer,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerCloseButton,
+	DrawerHeader,
+	DrawerBody,
+	useDisclosure,
 } from '@chakra-ui/react';
 import { MdMenu } from 'react-icons/md';
 
 import { RixaEmblem, ToggleColorMode } from '@root/components';
 
 interface HeaderMenuProps {
-	drawer?: boolean;
+	auth?: boolean;
 }
 
-export const HeaderMenu: React.FC<HeaderMenuProps> = ({ drawer = false }) => {
+export const HeaderMenu: React.FC<HeaderMenuProps> = () => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const menuRef = React.useRef();
+
 	return (
 		<>
 			<Box w="100%" h="60px" p={2} mb="3rem">
 				<Flex align="center" justifyContent="space-between">
 					<Menu>
 						<Flex align="center">
-							<MenuButton as={Button} size="sm">
+							<MenuButton as={Button} size="sm" ref={menuRef} onClick={onOpen}>
 								<Icon as={MdMenu} w="26px" h="26px" />
 							</MenuButton>
 							<ToggleColorMode />
 						</Flex>
 
-						<MenuList>
-							<MenuItem>Download</MenuItem>
-							<MenuItem>Create a Copy</MenuItem>
-							<MenuItem>Mark as Draft</MenuItem>
-							<MenuItem>Delete</MenuItem>
-							<MenuItem>Attend a Workshop</MenuItem>
-						</MenuList>
+						<Drawer
+							isOpen={isOpen}
+							placement="left"
+							onClose={onClose}
+							finalFocusRef={menuRef}
+						>
+							<DrawerOverlay>
+								<DrawerContent>
+									<DrawerCloseButton />
+									<DrawerHeader>Create your account</DrawerHeader>
+
+									<DrawerBody>hello world</DrawerBody>
+								</DrawerContent>
+							</DrawerOverlay>
+						</Drawer>
 					</Menu>
 
 					<ButtonGroup isAttached>
