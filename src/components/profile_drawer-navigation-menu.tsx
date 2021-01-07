3256fa72c623e2aace97d835f4e9deb5e1f26555
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2021-01-07 19:34:40 -0300

    :wrench: include rixa coins balance on profile avatar

--- src/components/profile_drawer-navigation-menu.tsx
+++ src/components/profile_drawer-navigation-menu.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import {
 	Button,
+	Icon,
+	Text,
 	Drawer,
 	DrawerOverlay,
 	DrawerContent,
@@ -11,6 +13,10 @@ import {
 	Avatar,
 	useDisclosure,
 } from '@chakra-ui/react';
+
+import { ImCoinYen } from 'react-icons/im';
+
+import { CustomBox } from '@root/components';
 
 interface DrawerNavigationItem {
 	label: string;
@@ -48,7 +54,26 @@ export const ProfileDrawerNavigationMenu: React.FC = () => {
 
 	return (
 		<>
-			<Button variant="unstyled" ref={avatarRef} onClick={onOpen}>
+			<Button
+				variant="unstyled"
+				ref={avatarRef}
+				onClick={onOpen}
+				display="flex"
+				alignItems="center"
+				position="relative"
+			>
+				<CustomBox
+					borderRadius="8px"
+					position="absolute"
+					pr="50px"
+					pl="2"
+					right="1rem"
+					display="flex"
+					alignItems="center"
+				>
+					<Icon as={ImCoinYen} mr="md" />
+					<Text fontSize="16px">1500</Text>
+				</CustomBox>
 				<Avatar
 					boxShadow="dark-lg"
 					src="https://opgg-static.akamaized.net/images/profile_icons/profileIcon4404.jpg?image=q_auto:best&v=1518361200"
