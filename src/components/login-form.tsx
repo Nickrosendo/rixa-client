cf2c348b76e5ae95307a2a0ab3835aa8f220b40b
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2021-01-08 16:41:23 -0300

    :wrench: include social connect button to auth forms

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -2,24 +2,51 @@ import React from 'react';
 
 import {
 	VStack,
+	HStack,
+	StackDivider,
+	Text,
+	Button,
+	Icon,
 	FormControl,
 	FormLabel,
 	Input,
 	Checkbox,
+	Square,
+	Flex,
 } from '@chakra-ui/react';
+
+import { FcGoogle } from 'react-icons/fc';
+import { ImFacebook2 } from 'react-icons/im';
 
 export const LoginForm: React.FC = () => {
 	return (
-		<VStack>
-			<FormControl id="email">
-				<FormLabel>Email address</FormLabel>
-				<Input type="email" />
-			</FormControl>
-			<FormControl id="password">
-				<FormLabel>Password</FormLabel>
-				<Input type="password" />
-			</FormControl>
-			<Checkbox defaultIsChecked>Keep connected</Checkbox>
-		</VStack>
+		<HStack spacing={4} align="start" divider={<StackDivider />}>
+			<VStack>
+				<FormControl id="email">
+					<FormLabel>Email address</FormLabel>
+					<Input type="email" />
+				</FormControl>
+				<FormControl id="password">
+					<FormLabel>Password</FormLabel>
+					<Input type="password" />
+				</FormControl>
+				<Checkbox defaultIsChecked>Keep connected</Checkbox>
+			</VStack>
+			<Flex flexDir="column" justifyContent="center" w="50%">
+				<Text textAlign="left"> Social connect </Text>
+				<Button justifyContent="start" my={2}>
+					<Square bg="white" borderRadius="8px" p="1px">
+						<Icon as={FcGoogle} />
+					</Square>
+					<Text ml={1}> Google </Text>
+				</Button>
+				<Button justifyContent="start">
+					<Square bg="white" borderRadius="8px">
+						<Icon as={ImFacebook2} color="blue.400" />
+					</Square>
+					<Text ml={1}> Facebook </Text>
+				</Button>
+			</Flex>
+		</HStack>
 	);
 };
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import {
 	VStack,
 	HStack,
@@ -8,34 +7,73 @@ import {
 	FormLabel,
 	Input,
 	FormHelperText,
+	Text,
+	Button,
+	Icon,
+	Square,
+	Flex,
 } from '@chakra-ui/react';
+import { FcGoogle } from 'react-icons/fc';
+import { ImFacebook2 } from 'react-icons/im';
 
 export const SignupForm: React.FC = () => {
 	return (
-		<VStack>
-			<FormControl id="email">
-				<FormLabel>Email address</FormLabel>
-				<Input type="email" />
-			</FormControl>
+		<VStack spacing={4} align="start" divider={<StackDivider />}>
+			<Flex
+				flexDir="column"
+				justifyContent="center"
+				w="100%"
+				alignItems="start"
+			>
+				<Text textAlign="left" mb={2}>
+					{' '}
+					Register with social network{' '}
+				</Text>
+				<Flex alignItems="center">
+					<Button justifyContent="start" mx={2}>
+						<Square bg="white" borderRadius="8px" p="1px">
+							<Icon as={FcGoogle} />
+						</Square>
+						<Text ml={1}> Google </Text>
+					</Button>
+					<Button justifyContent="start">
+						<Square bg="white" borderRadius="8px">
+							<Icon as={ImFacebook2} color="blue.400" />
+						</Square>
+						<Text ml={1}> Facebook </Text>
+					</Button>
+				</Flex>
+			</Flex>
 
-			<FormControl id="nickname">
-				<FormLabel>Nickname</FormLabel>
-				<Input type="text" />
-				<FormHelperText>
-					That will be how other players find you.
-				</FormHelperText>
-			</FormControl>
+			<VStack align="start">
+				<Text textAlign="left" mb={2}>
+					{' '}
+					Register with email and password{' '}
+				</Text>
+				<FormControl id="email">
+					<FormLabel>Email address</FormLabel>
+					<Input type="email" />
+				</FormControl>
 
-			<HStack divider={<StackDivider />} spacing={4}>
-				<FormControl id="password">
-					<FormLabel>Password</FormLabel>
-					<Input type="password" />
+				<FormControl id="nickname">
+					<FormLabel>Nickname</FormLabel>
+					<Input type="text" />
+					<FormHelperText>
+						That will be how other players find you.
+					</FormHelperText>
 				</FormControl>
-				<FormControl id="confirm_password">
-					<FormLabel>Confirm password</FormLabel>
-					<Input type="password" />
-				</FormControl>
-			</HStack>
+
+				<HStack divider={<StackDivider />} spacing={4}>
+					<FormControl id="password">
+						<FormLabel>Password</FormLabel>
+						<Input type="password" />
+					</FormControl>
+					<FormControl id="confirm_password">
+						<FormLabel>Confirm password</FormLabel>
+						<Input type="password" />
+					</FormControl>
+				</HStack>
+			</VStack>
 		</VStack>
 	);
 };
